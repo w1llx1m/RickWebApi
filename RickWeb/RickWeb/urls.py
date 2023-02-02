dc5b4f0d9557f45abc0ec95cd154b66ed4a9fc64
w1llx1m <williamsilva.monteiro28@gmail.com>
2023-02-02 11:26:47 -0300

    acessando o json

--- RickWeb/RickWeb/urls.py
+++ RickWeb/RickWeb/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from characters.views import characters_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),

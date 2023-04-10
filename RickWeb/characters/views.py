c7d2630c2da6e1fc4bcc1747994a92b13fc252c4
w1llx1m <williamsilva.monteiro28@gmail.com>
2023-04-10 18:34:42 -0300

    add the user model

--- RickWeb/characters/views.py
+++ RickWeb/characters/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+import json
 import requests as rq
 
 def characters_view(request, *args, **kwargs):
-    content = rq.get("https://rickandmortyapi.com/api/character")
-    context = {'characters': content.content}
+    content = get_all_characters()
+    context = {'characters': content}
     return render(request, "characters.html", context)
+
+
+def get_all_characters():
+    ret = rq.get("https://rickandmortyapi.com/api/character")
+    jsonRet = json.loads(ret.content.decode('utf-8'))
+    for x in jsonRet["results"]:
+        pass
+        #print(x["image"])
+    return jsonRet
